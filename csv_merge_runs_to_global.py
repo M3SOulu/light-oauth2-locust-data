b7bd7cd4264ae0faf90a5e2e21ee8f10384de39e
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-10-18 12:18:41 +0300

    fix: concat dfs explicitly

--- csv_merge_runs_to_global.py
+++ csv_merge_runs_to_global.py
@@ -5,11 +5,11 @@ run_dfs = []
 for run_name in os.listdir(os.getcwd()):
     if not os.path.isdir(run_name) or not run_name.startswith("light-oauth2"):
         continue
-    run_dfs.append(f"{run_name}.csv")
+    run_dfs.append(pl.scan_csv(f"{run_name}.csv", infer_schema=False))
     print(f"Added {run_name}.csv")
 
 print("Scanning csv data")
-result = pl.scan_csv(run_dfs, infer_schema=False)
+result = pl.concat(run_dfs, how='vertical')
 print("Sorting all data by timestamp")
 result = result.sort(by=['timestamp'])
 print("Saving light-oauth2-data.csv")
