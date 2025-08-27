48b0bcf8eb422b8bcf94a4fc311a6c2d27aca378
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2025-08-27 10:52:56 +0300

    add: write container metrics to separate csvs

--- csv_generator.py
+++ csv_generator.py
@@ -7,6 +7,7 @@ for run_dir in os.listdir(os.getcwd()):
                 run_dir.startswith('LO2_run')]):
         continue
     NODE_METRICS = dict()
+    CONTAINER_METRICS = dict()
     node_output_csv = f"{run_dir}-node-metrics.csv"
     for test_dir in os.listdir(run_dir):
         test_path = os.path.join(run_dir, test_dir)
@@ -31,6 +32,16 @@ for run_dir in os.listdir(os.getcwd()):
                         test_dict = NODE_METRICS.setdefault(timestamp, dict())
                         metric_dict = test_dict.setdefault(test_dir, dict())
                         metric_dict[metric_name] = value
+                elif metric["metric"]["job"] == "cadvisor":
+                    if "name" not in metric["metric"]:
+                        continue
+                    container_name = metric["metric"]["name"]
+                    metric_name = metric["metric"]["__name__"]
+                    container_dict = CONTAINER_METRICS.setdefault(container_name, dict())
+                    for timestamp, value in metric["values"]:
+                        test_dict = container_dict.setdefault(timestamp, dict())
+                        metric_dict = test_dict.setdefault(test_dir, dict())
+                        metric_dict[metric_name] = value
 
     if os.path.isfile(node_output_csv):
         print(f"{node_output_csv} exists, skipping...")
@@ -53,3 +64,25 @@ for run_dir in os.listdir(os.getcwd()):
                     for metric in all_metrics:
                         row.append(metrics_dict.get(metric, None))
                 csv_writer.writerow(row)
+
+    for container_name, container_dict in CONTAINER_METRICS.items():
+        container_output_csv = f"{run_dir}-{container_name}-metrics.csv"
+        all_metrics = set()
+        for test_dict in container_dict.values():
+            for metric_dict in test_dict.values():
+                all_metrics = all_metrics.union(metric_dict.keys())
+        all_metrics = sorted(list(all_metrics))
+        HEADER = ["timestamp", "run", "container", "test"]
+        HEADER.extend(all_metrics)
+
+        print(f"Writing to {container_output_csv}...")
+        with open(container_output_csv, 'w', newline='') as f:
+            csv_writer = csv.writer(f)
+            csv_writer.writerow(HEADER)
+            for timestamp, test_dict in container_dict.items():
+                for test_name, metrics_dict in test_dict.items():
+                    row = [timestamp, run_dir, container_name, test_name]
+                    for metric in all_metrics:
+                        row.append(metrics_dict.get(metric, None))
+                csv_writer.writerow(row)
+
