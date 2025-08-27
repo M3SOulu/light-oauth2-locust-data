b39fd4672f6b03b80f134c9743f233772eaf0654
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2025-08-27 10:32:25 +0300

    add: put node metrics for all tests in the same csv for a run

--- csv_generator.py
+++ csv_generator.py
@@ -6,15 +6,15 @@ for run_dir in os.listdir(os.getcwd()):
     if not all([os.path.isdir(run_dir),
                 run_dir.startswith('LO2_run')]):
         continue
+    METRICS = dict()
+    output_csv = f"{run_dir}.csv"
     for test_dir in os.listdir(run_dir):
         test_path = os.path.join(run_dir, test_dir)
-        output_csv = f"{run_dir}-{test_dir}.csv"
         if os.path.isfile(output_csv):
             print(f"{output_csv} exists, skipping...")
             continue
         if not os.path.isdir(test_path):
             continue
-        METRICS = dict()
         metrics_dir = os.path.join(test_path, "metrics")
         for metric_file in os.listdir(metrics_dir):
             if os.path.splitext(metric_file)[1] != ".json":
@@ -32,22 +32,25 @@ for run_dir in os.listdir(os.getcwd()):
                 metric_name = metric["metric"]["__name__"] + "&" + "&".join([f"{k}={v}" for k,v in metric["metric"].items() if k not in {"group", "instance", "job", "__name__"}])
                 metric_name = metric_name.removesuffix("&")
                 for timestamp, value in metric["values"]:
-                    metric_values = METRICS.setdefault(timestamp, dict())
-                    metric_values[metric_name] = value
+                    test_dict = METRICS.setdefault(timestamp, dict())
+                    metric_dict = test_dict.setdefault(test_dir, dict())
+                    metric_dict[metric_name] = value
 
-        all_metrics = set()
-        for timestamp, metrics_dir in METRICS.items():
-            all_metrics = all_metrics.union(metrics_dir.keys())
-        all_metrics = sorted(list(all_metrics))
-        HEADER = ["timestamp", "run_start", "test_name"]
-        HEADER.extend(all_metrics)
+    all_metrics = set()
+    for test_dict in METRICS.values():
+        for metric_dict in test_dict.values():
+            all_metrics = all_metrics.union(metric_dict.keys())
+    all_metrics = sorted(list(all_metrics))
+    HEADER = ["timestamp", "run_start", "test_name"]
+    HEADER.extend(all_metrics)
 
-        print(f"Writing to {output_csv}...")
-        with open(output_csv, 'w', newline='') as f:
-            csv_writer = csv.writer(f)
-            csv_writer.writerow(HEADER)
-            for timestamp, metrics_dir in METRICS.items():
-                row = [timestamp, run_dir, test_dir]
+    print(f"Writing to {output_csv}...")
+    with open(output_csv, 'w', newline='') as f:
+        csv_writer = csv.writer(f)
+        csv_writer.writerow(HEADER)
+        for timestamp, test_dict in METRICS.items():
+            for test_name, metrics_dict in test_dict.items():
+                row = [timestamp, run_dir, test_name]
                 for metric in all_metrics:
-                    row.append(metrics_dir.get(metric, None))
-                csv_writer.writerow(row)
+                    row.append(metrics_dict.get(metric, None))
+            csv_writer.writerow(row)
